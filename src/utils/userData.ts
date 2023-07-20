9b856013795e3a609e38fcacff121490c46d9e9a
Brunohgs21 <brunohgs21@outlook.com>
2023-07-20 14:31:52 -0300

    feat: adding my info

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Bruno Henrique",
+  githubUser: "Brunohgs21",
+  linkedinUser: "brunohgsantos",
+  whatsappNumber: "+5581981456272",
+  emailUser: "brunohgs21@outlook.com",
+  instagramUser: "",
   facebookUser: "",
 };
